3a041d3c7e556b045f7a109e0432f91980597fc1
DeepLakePR <guilhermefortunato671@gmail.com>
2025-02-08 17:12:51 -0300

    feat: finish gallery page, responsive and design improvements

--- public/js/home.js
+++ public/js/home.js
@@ -9,22 +9,6 @@ $(() => {
     }
 
     ///////////////////////////
-    // Smooth Scrolling
-    const lenis = new Lenis({
-        duration: 2,
-        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-        smoothWheel: true,
-        smoothTouch: true,
-    });
-
-    function lenis_raf(time) {
-        lenis.raf(time);
-        requestAnimationFrame(lenis_raf);
-    }
-
-    requestAnimationFrame(lenis_raf);
-
-    ///////////////////////////
     // Apresentation Carousel
     const SectionApresentation = $('section.apresentation');
 
@@ -163,28 +147,5 @@ $(() => {
         return; 
 
     });
-
-    ///////////////////////////
-    // Footer Common Questions
-    const CommonQuestion_Single = $('footer .com-quest');
-
-    CommonQuestion_Single.each((_, questionDiv) => {
-
-        questionDiv = $(questionDiv);
-
-        questionDiv.find('p, .com-quest-content').click(() => {
-
-            if (typeof questionDiv.attr('opened') !== 'undefined' && questionDiv.attr('opened') !== false) {
-                questionDiv.removeAttr('opened');
-                return;
-
-            }
-
-            questionDiv.attr('opened', '');
-            return;
-
-        });
-
-    })
 
 });
